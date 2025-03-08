8a5632be23ff7a4fbd45a33b9f3a2f2ed157beee
AlexeyGretsov <58134097+AlexeyGretsov@users.noreply.github.com>
2025-03-09 00:01:32 +0300

    Impl lexicographical sort

--- main.cc
+++ main.cc
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
 int main(int argc, char *argv[]) {
 	std::cout << "Hello, World!" << std::endl;
+
+	std::vector<std::string> ips = {"111.2.3.4", "111.3.1.1", "111.2.2.1", "111.2.3.3", "127.0.0.1", "11.3.4.5"};
+
+	std::sort(ips.begin(), ips.end(), 
+		[](const std::string &a, const std::string &b) -> bool
+		{
+			return std::lexicographical_compare(a.begin(), a.end(), b.begin(), b.end());
+		});
+
+	for(auto &ip : ips)
+	{
+		std::cout << "IP: " << ip << std::endl;
+	}
 
 	return 0;
 }
