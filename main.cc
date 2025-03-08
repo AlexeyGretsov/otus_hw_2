2ebbc324a4dd68a47fac7c3decb43767c62cf059
AlexeyGretsov <58134097+AlexeyGretsov@users.noreply.github.com>
2025-03-09 00:08:45 +0300

    Format files

--- main.cc
+++ main.cc
@@ -1,23 +1,23 @@
-#include <iostream>
-#include <vector>
-#include <string>
 #include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
 
 int main(int argc, char *argv[]) {
-	std::cout << "Hello, World!" << std::endl;
+  std::cout << "Hello, World!" << std::endl;
 
-	std::vector<std::string> ips = {"111.2.3.4", "111.3.1.1", "111.2.2.1", "111.2.3.3", "127.0.0.1", "11.3.4.5"};
+  std::vector<std::string> ips = {"111.2.3.4", "111.3.1.1", "111.2.2.1",
+                                  "111.2.3.3", "127.0.0.1", "11.3.4.5"};
 
-	std::sort(ips.begin(), ips.end(), 
-		[](const std::string &a, const std::string &b) -> bool
-		{
-			return std::lexicographical_compare(a.begin(), a.end(), b.begin(), b.end());
-		});
+  std::sort(ips.begin(), ips.end(),
+            [](const std::string &a, const std::string &b) -> bool {
+              return std::lexicographical_compare(a.begin(), a.end(), b.begin(),
+                                                  b.end());
+            });
 
-	for(auto &ip : ips)
-	{
-		std::cout << "IP: " << ip << std::endl;
-	}
+  for (auto &ip : ips) {
+    std::cout << "IP: " << ip << std::endl;
+  }
 
-	return 0;
+  return 0;
 }
